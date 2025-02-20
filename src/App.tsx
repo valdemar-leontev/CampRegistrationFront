407c375c66eac95187a70423060fd544afb35140
valdemar-leontev <vladimi.leontev@x5.ru>
2025-02-20 15:11:49 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,19 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+} from "@/components/ui/table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Drawer,
   DrawerClose,
   DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
   DrawerDescription,
   DrawerFooter,
-  DrawerHeader,
-  DrawerTitle
-} from "@/components/ui/drawer"
+} from "@/components/ui/drawer";
 import { Button } from './components/ui/button';
-
-
+import { motion } from 'framer-motion';
 
 interface Invoice {
   lastName: string;
@@ -42,28 +41,31 @@ const invoices: Invoice[] = [
   { lastName: 'Попов', firstName: 'Анатолий', middleName: 'Анатолиевич', age: 33, city: 'Челябинск', church: 'Церковь Святой Троицы', paymentStatus: 'Оплачено' },
   { lastName: 'Васильев', firstName: 'Сергей', middleName: 'Сергеевич', age: 29, city: 'Нижний Новгород', church: 'Церковь Святого Георгия', paymentStatus: 'Оплачено' },
   { lastName: 'Зайцев', firstName: 'Олег', middleName: 'Олегович', age: 31, city: 'Ростов-на-Дону', church: 'Церковь Святого Иоанна', paymentStatus: 'Не оплачено' },
-  { lastName: 'Иванов', firstName: 'Иван', middleName: 'Иванович', age: 30, city: 'Москва', church: 'Церковь Святого Петра', paymentStatus: 'Скинул скрин' },
-  { lastName: 'Петров', firstName: 'Петр', middleName: 'Петрович', age: 25, city: 'Санкт-Петербург', church: 'Церковь Спаса', paymentStatus: 'Не оплачено' },
-  { lastName: 'Сидоров', firstName: 'Сидор', middleName: 'Сидорович', age: 35, city: 'Новосибирск', church: 'Церковь Успения', paymentStatus: 'Скинул скрин' },
-  { lastName: 'Кузнецов', firstName: 'Алексей', middleName: 'Алексеевич', age: 40, city: 'Екатеринбург', church: 'Церковь Святого Николая', paymentStatus: 'Оплачено' },
-  { lastName: 'Смирнов', firstName: 'Дмитрий', middleName: 'Дмитриевич', age: 28, city: 'Казань', church: 'Церковь Казанской иконы', paymentStatus: 'Не оплачено' },
-  { lastName: 'Попов', firstName: 'Анатолий', middleName: 'Анатолиевич', age: 33, city: 'Челябинск', church: 'Церковь Святой Троицы', paymentStatus: 'Оплачено' },
-  { lastName: 'Васильев', firstName: 'Сергей', middleName: 'Сергеевич', age: 29, city: 'Нижний Новгород', church: 'Церковь Святого Георгия', paymentStatus: 'Оплачено' },
-  { lastName: 'Зайцев', firstName: 'Олег', middleName: 'Олегович', age: 31, city: 'Ростов-на-Дону', church: 'Церковь Святого Иоанна', paymentStatus: 'Не оплачено' },
 ];
 
 const App: FC = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+  const [selectedInvoice, setSelectedInvoice] = useState<Invoice | null>(null);
+
+  const handleRowClick = (invoice: Invoice) => {
+    setSelectedInvoice(invoice);
+    setIsDrawerOpen(true);
+  };
+
+  const handlePaymentConfirmation = () => {
+    if (selectedInvoice) {
+      console.log(`Оплата подтверждена для: ${selectedInvoice.firstName} ${selectedInvoice.lastName}`);
+      setIsDrawerOpen(false);
+    }
+  };
 
   return (
-    <>
-      <Tabs defaultValue="account">
-        <TabsList >
+    <div className='bg-white'>
+      <Tabs defaultValue="Детский">
+        <TabsList>
           <TabsTrigger value="Детский">Детский</TabsTrigger>
           <TabsTrigger value="Подростковый">Подростковый</TabsTrigger>
-          <TabsTrigger value="Отец и сын">Отец и сын</TabsTrigger> 
-          {/* <TabsTrigger value="Семейный">Семейный</TabsTrigger> */}
-          {/* <TabsTrigger value="Молодежный">Молодежный</TabsTrigger>  */}
+          <TabsTrigger value="Отец и сын">Отец и сын</TabsTrigger>
         </TabsList>
         <TabsContent value="Детский">
           <div>
@@ -81,41 +83,60 @@ const App: FC = () => {
               </TableHeader>
               <TableBody>
                 {invoices.map((invoice, index) => (
-                  <TableRow key={index} className="border-b hover:bg-gray-50 transition-colors duration-200">
-                    <TableCell className="py-2 px-4" onClick={() => setIsDrawerOpen(true)}>{invoice.lastName}</TableCell>
-                    <TableCell className="py-2 px-4" onClick={() => setIsDrawerOpen(true)}>{invoice.firstName}</TableCell>
-                    <TableCell className="py-2 px-4" onClick={() => setIsDrawerOpen(true)}>{invoice.middleName}</TableCell>
-                    <TableCell className="py-2 px-4" onClick={() => setIsDrawerOpen(true)}>{invoice.age}</TableCell>
-                    <TableCell className="py-2 px-4" onClick={() => setIsDrawerOpen(true)}>{invoice.city}</TableCell>
-                    <TableCell className="py-2 px-4" onClick={() => setIsDrawerOpen(true)}>{invoice.church}</TableCell>
-                    <TableCell className="py-2 px-4" onClick={() => setIsDrawerOpen(true)}>{invoice.paymentStatus}</TableCell>
+                  <TableRow 
+                    key={index} 
+                    className="border-b hover:bg-gray-50 transition-colors duration-200 cursor-pointer" 
+                    onClick={() => handleRowClick(invoice)}
+                  >
+                    <TableCell className="py-2 px-4">{invoice.lastName}</TableCell>
+                    <TableCell className="py-2 px-4">{invoice.firstName}</TableCell>
+                    <TableCell className="py-2 px-4">{invoice.middleName}</TableCell>
+                    <TableCell className="py-2 px-4 text-right">{invoice.age}</TableCell>
+                    <TableCell className="py-2 px-4">{invoice.city}</TableCell>
+                    <TableCell className="py-2 px-4">{invoice.church}</TableCell>
+                    <TableCell className="py-2 px-4">{invoice.paymentStatus}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
 
-            <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen} >
+            <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
               <DrawerContent>
-                <DrawerHeader>
-                  <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-                  <DrawerDescription>This action cannot be undone.</DrawerDescription>
-                </DrawerHeader>
-                <DrawerFooter>
-                  <Button>Submit</Button>
-                  <DrawerClose>
-                    <Button variant="outline">Cancel</Button>
-                  </DrawerClose>
-                </DrawerFooter>
+                <motion.div
+                  initial={{ scale: 0.5, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  exit={{ scale: 0.5, opacity: 0 }}
+                  transition={{ type: 'spring', stiffness: 50 }}
+                >
+                  <DrawerHeader>
+                    <DrawerTitle>Информация о счете</DrawerTitle>
+                    {selectedInvoice && (
+                      <DrawerDescription className='pt-6'>
+                        <div className='text-left'>
+                          <p><strong>Фамилия:</strong> {selectedInvoice.lastName}</p>
+                          <p><strong>Имя:</strong> {selectedInvoice.firstName}</p>
+                          <p><strong>Отчество:</strong> {selectedInvoice.middleName}</p>
+                          <p><strong>Возраст:</strong> {selectedInvoice.age}</p>
+                          <p><strong>Город:</strong> {selectedInvoice.city}</p>
+                          <p><strong>Церковь:</strong> {selectedInvoice.church}</p>
+                          <p><strong>Статус оплаты:</strong> {selectedInvoice.paymentStatus}</p>
+                        </div>
+                      </DrawerDescription>
+                    )}
+                  </DrawerHeader>
+                  <DrawerFooter>
+                    <Button onClick={handlePaymentConfirmation} variant="outline">Подтвердить оплату</Button>
+                    <DrawerClose asChild>
+                      <Button variant="outline">Закрыть</Button>
+                    </DrawerClose>
+                  </DrawerFooter>
+                </motion.div>
               </DrawerContent>
             </Drawer>
           </div>
         </TabsContent>
       </Tabs>
-
-
-
-    </>
-
+    </div>
   );
 };
 
