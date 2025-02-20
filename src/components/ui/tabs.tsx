746a83d4d9a073727d3e73bd63ce7894dcf596c8
valdemar-leontev <vladimi.leontev@x5.ru>
2025-02-20 14:08:57 +0300

    add rollup

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 
 import { cn } from "@/lib/utils"
+import React from 'react'
 
 const Tabs = TabsPrimitive.Root
 
