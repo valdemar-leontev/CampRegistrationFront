932e086b58cfe75677fbb572009d2260df93bc65
valdemar-leontev <vladimi.leontev@x5.ru>
2025-02-22 21:13:08 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
 
-import { init, setMiniAppHeaderColor, setMiniAppBackgroundColor } from '@telegram-apps/sdk-react';
-import { setHeaderColor } from 'node_modules/@telegram-apps/sdk/dist/dts/scopes/components/mini-app/methods.js';
+import { init } from '@telegram-apps/sdk-react';
+
 
 init();
-
-
-setMiniAppBackgroundColor('bg_color')
-setMiniAppHeaderColor('bg_color')
-
-if (setHeaderColor.isAvailable() && setHeaderColor.supports.rgb()) {
-  setHeaderColor('#ffaabb');
-  }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
