6bfbfde0f1944db01a25ef460368cc88ee627d40
valdemar-leontev <vladimi.leontev@x5.ru>
2025-02-27 10:57:33 +0300

    fix

--- src/components/appComponents/drawer-registration.tsx
+++ src/components/appComponents/drawer-registration.tsx
@@ -113,7 +113,7 @@ export function DrawerRegistration() {
       </Button>
 
       <Drawer open={isOpen} onOpenChange={setIsOpen}>
-        <DrawerContent ref={formContainerRef} className="h-[100vh] min-h-[100vh] overflow-auto transition-all duration-1000 fixed ">
+        <DrawerContent ref={formContainerRef} className="h-[100vh] min-h-[100vh] overflow-auto transition-all duration-1000 fixed p-5">
 
           <DrawerHeader>
             <DrawerTitle className="text-xl font-semibold">{steps[step]}</DrawerTitle>
