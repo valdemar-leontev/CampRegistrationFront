c86f61ec4b1b4f38abbfdc5dcd8e8ae4bc7049e5
valdemar-leontev <vladimi.leontev@x5.ru>
2025-02-28 10:55:48 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
+// import PoweredByGod from './components/appComponents/powered-by-God.js';
 import { init } from '@telegram-apps/sdk-react'
 import { retrieveRawInitData } from '@telegram-apps/bridge';
+import { NavigationPanels } from './components/appComponents/navigation-panels.js';
+import { ProfileBar } from './components/appComponents/profile-bar.js';
 
 
 init()
@@ -27,8 +30,11 @@ const Root = () => {
     <StrictMode>
       {/* <PoweredByGod /> */}
 
+      <ProfileBar user={user} />
+
       <App user={user} />
 
+      <NavigationPanels />
 
     </StrictMode>
   );
